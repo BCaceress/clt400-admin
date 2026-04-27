b44d01fddd42f7759e7a937463df847bca3091b7
Bruno Caceres <brunocaceres@live.com>
2026-04-27 09:43:42 -0300

    Login, icone

---
+++ types/lucide-react.d.ts
@@ -0,0 +1,24 @@
+declare module 'lucide-react' {
+  import { FC, SVGProps } from 'react';
+
+  type LucideIcon = FC<SVGProps<SVGSVGElement> & { size?: number | string }>;
+
+  export const LogIn: LucideIcon;
+  export const LogOut: LucideIcon;
+  export const Loader2: LucideIcon;
+  export const ArrowLeft: LucideIcon;
+  export const Save: LucideIcon;
+  export const Trash2: LucideIcon;
+  export const Check: LucideIcon;
+  export const Copy: LucideIcon;
+  export const AlertTriangle: LucideIcon;
+  export const X: LucideIcon;
+  export const RefreshCw: LucideIcon;
+  export const Power: LucideIcon;
+  export const Edit: LucideIcon;
+  export const Plus: LucideIcon;
+  export const Server: LucideIcon;
+
+  const icons: Record<string, LucideIcon>;
+  export default icons;
+}
